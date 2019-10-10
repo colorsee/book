d763f39ba3c375b16c245e22e4e613b7e1ac3f47
Idan Loo <im@siwei.lu>
2019-10-10 23:37:03 +0800

    fix: parse timestamps incorrectly

--- src/utils/datetime.js
+++ src/utils/datetime.js
@@ -2,5 +2,12 @@
 import dayjs from 'dayjs'
 
 export default function datetime(date: string) {
+  date = '' + date
+
+  if (date.length < 13) {
+    date += '000'
+  }
+
+  date = parseInt(date)
   return dayjs(date).format('YYYY-MM-DD HH:mm:ss')
 }
