6e6a9b1888fa43af0b9e69a04755649838c3f4de
Idan Loo <im@siwei.lu>
2019-10-11 00:31:32 +0800

    fix: cannot add underline in firefox

--- src/utils/getSelection.js
+++ src/utils/getSelection.js
@@ -3,7 +3,9 @@ export default function getSelection() {
   const sel = document.getSelection()
   const value = sel.toString()
 
-  const { anchorNode, anchorOffset, focusNode, extentOffset } = sel
+  console.log(sel)
+
+  const { anchorNode, anchorOffset, focusNode, focusOffset } = sel
   const anchorPart = $(anchorNode).parent()
   const extentPart = $(focusNode).parent()
   const [article] = $(anchorPart).prevAll('[id]')
@@ -12,7 +14,7 @@ export default function getSelection() {
   let start_part = anchorPart.data('partcode')
   let end_part = extentPart.data('partcode')
   let start_word = anchorOffset
-  let end_word = extentOffset
+  let end_word = focusOffset
 
   if (start_part > end_part) {
     ;[start_part, end_part] = [end_part, start_part]
